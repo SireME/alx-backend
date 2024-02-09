5bf7c5c2703183e82ad4c54a385ed7522c023770
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-09 12:38:34 +0100

    implement locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,8 +3,7 @@
 Basic Flask app with basic i18n configs
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-import os
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
