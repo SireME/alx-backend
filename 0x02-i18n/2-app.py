956cf8d0e89faa7a4f9347abf9aac3b1f3e30ba3
Mbang Ernest N <mbangernest@yahoo.com>
2024-02-09 12:53:35 +0100

    add type annotation

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -26,8 +26,7 @@ def get_locale() -> str:
     """
     determine best match for users preferrred lanaguage
     """
-    rq = request.accept_languages
-    return rq.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
